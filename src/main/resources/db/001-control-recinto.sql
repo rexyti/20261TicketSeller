933eace32ff1d556272dae643707986129eb7524
Nestor Saumeth <nisaumeth@unimagdalena.edu.co>
2026-05-09 23:14:33 -0500

    Adding field tipo on table zonas.

--- src/main/resources/db/001-control-recinto.sql
+++ src/main/resources/db/001-control-recinto.sql
@@ -1,6 +1,6 @@
 CREATE TABLE recintos
 (
-    id                 UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id                 UUID PRIMARY KEY      DEFAULT gen_random_uuid(),
     nombre             VARCHAR(150) NOT NULL,
     ciudad             VARCHAR(100) NOT NULL,
     direccion          VARCHAR(255) NOT NULL,
@@ -19,7 +19,8 @@ CREATE TABLE zonas
     id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     recinto_id UUID         NOT NULL REFERENCES recintos (id),
     nombre     VARCHAR(100) NOT NULL,
-    capacidad  INTEGER      NOT NULL
+    capacidad  INTEGER      NOT NULL,
+    tipo       VARCHAR(20)  NOT NULL
 );
 
 CREATE TABLE compuertas
